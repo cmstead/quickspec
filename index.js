0205fc27f0ae45438c6979bb8e2204d0cbdb0060
Chris Stead <cmstead@gmail.com>
2017-09-29 17:07:39 -0700

    Initial library development

---
+++ index.js
@@ -0,0 +1,118 @@
+(function (moduleFactory) {
+    let isNode = typeof module !== undefined && typeof module.exports !== undefined
+
+    if (isNode) {
+        // const signet = require('./signet-types');
+
+        module.exports = moduleFactory();
+    } else if (typeof signet === 'object') {
+        window.quickspec = moduleFactory();
+    } else {
+        throw new Error('The module quickspec requires Signet to run.');
+    }
+
+})(function () {
+    'use strict';
+
+    return function buildQuickspec(options) {
+
+        const defaultOptions = {
+            verbose: true
+        };
+
+        const optionsExist = typeof options === 'object' && options !== null;
+        const testOptions = optionsExist ? options : defaultOptions;
+
+        const verboseLogging = Boolean(testOptions.verbose);
+
+        function getSpecName(specRecord) {
+            return typeof specRecord.name === 'string' ? specRecord.name : 'unnamed';
+        }
+
+        function buildFailureMessage(specRecord, expectedResult, actualResult) {
+            return 'Test \'' + getSpecName(specRecord) + '\' failure in spec: \n' +
+                JSON.stringify(specRecord.setupValues, null, 4) + '\n\n' +
+                'Expected result to be ' + expectedResult + ', but got ' + actualResult;
+        }
+
+        function logStatus(status, message) {
+            if (verboseLogging) {
+                console.log(`\t[quickspec:${status}] -- ${message}`);
+            }
+        }
+
+        function compareObjects(expectedResult, actualResult) {
+            let objectsOk = actualResult !== null 
+                && typeof expectedResult === typeof actualResult;
+
+            const expectedKeys = Object.keys(expectedResult);
+            const actualKeys = Object.keys(actualResult);
+
+            objectsOk = objectsOk && expectedKeys.length === actualKeys.length;
+
+            return expectedKeys.reduce(function (result, key) {
+                return objectsOk && compareResults(expectedResult[key], actualResult[key]);
+            }, objectsOk);
+        }
+
+        function compareResults(expectedResult, actualResult) {
+            return typeof expectedResult !== 'object' || expectedResult === null
+                ? expectedResult === actualResult
+                : compareObjects(expectedResult, actualResult);
+        }
+
+        function logResultStatus(specRecord, testResultOk) {
+            const status = testResultOk ? 'success' : 'failure';
+            const message = testResultOk
+                ? `Finished running '${getSpecName(specRecord)}' successfully`
+                : `Failed spec: ${getSpecName(specRecord)}`;
+
+            logStatus(status, message);
+        }
+
+        function throwOnFailure(testResultOk, specRecord, expectedResult, actualResult) {
+            if (!testResultOk) {
+                throw new Error(buildFailureMessage(specRecord, expectedResult, actualResult));
+            }
+        }
+
+        const throwOnFailureBuilder = (specRecord, theorem) => (actualResult) => {
+            const expectedResult = theorem(specRecord.setupValues);
+            const testResultOk = compareResults(expectedResult, actualResult);
+
+            logResultStatus(specRecord, testResultOk);
+            throwOnFailure(testResultOk, specRecord, expectedResult, actualResult);
+        }
+
+        const always = (value) => () => value;
+
+        function runVerification(testRunner, specSet, theorem) {
+            specSet.forEach(function (specRecord) {
+                const localTheorem = typeof theorem === 'function' ? theorem : always(specRecord.expectedResult);
+                const resolveTestResult = throwOnFailureBuilder(specRecord, localTheorem);
+
+                testRunner(specRecord.setupValues, resolveTestResult);
+            });
+        }
+
+
+        const verifyWithTheorem = (testRunner) => (theorem) => {
+            return {
+                over: (specSet) => runVerification(testRunner, specSet, theorem)                
+            }
+        }
+
+        function verify(testRunner) {
+
+            const nextActions = verifyWithTheorem(testRunner)(null);
+            nextActions.withTheorem = verifyWithTheorem(testRunner);
+
+            return nextActions;
+        }
+
+        return {
+            verify: verify
+        };
+    }
+
+});
---
+++ test/quickspec.test.js
@@ -0,0 +1,140 @@
+'use strict';
+
+const assert = require('chai').assert;
+
+const quickspec = require('../index.js')({
+    verbose: false
+});
+
+describe('quickspec', function () {
+
+    // This add is supposed to misbehave
+    const add = (a) => (b) => {
+        b = typeof b === 'number' ? b : 0;
+        return a + b;
+    }
+
+    function executeAdd({ value1, value2 }, verify) {
+        verify(add(value1)(value2));
+    }
+
+    function copyObject(obj) {
+        const valueIsArray = Object.prototype.toString.call(obj) === '[object Array]';
+        let result = valueIsArray ? [] : {};
+
+        return Object.keys(obj).reduce(function (result, key) {
+            result[key] = copyValue(obj[key]);
+            return result;
+        }, result);
+    }
+
+    function copyValue(value) {
+        return typeof value === 'object' && value !== null
+            ? copyObject(value)
+            : value;
+    }
+
+    function executeIdentity({ value }, verify) {
+        const maybeBadValue = Object.keys(value).length === 0 ? { 'test': 'test' } : value;
+        verify(maybeBadValue);
+    }
+
+    it('should verify all records in spec array including expected result and failure', function () {
+        const specSet = [
+            {
+                name: 'additive identity',
+                setupValues: { value1: 1, value2: 0 },
+                expectedResult: 1
+            },
+            {
+                name: 'add two numbers',
+                setupValues: { value1: 1, value2: 2 },
+                expectedResult: 3
+            },
+            {
+                name: 'incorrect value causes test failure',
+                setupValues: { value1: 1, value2: 0 },
+                expectedResult: 4
+            }
+        ];
+
+        var message = 'Test \'incorrect value causes test failure\' failure in spec: \n' +
+            JSON.stringify(specSet[2].setupValues, null, 4) + '\n\n' +
+            'Expected result to be 4, but got 1';
+
+        var runSpec = () => quickspec.verify(executeAdd).over(specSet);
+        assert.throws(runSpec, message);
+    });
+
+    it('should verify all records with a theorem and throw on failure', function () {
+
+        function addTheorem({ value1, value2 }) {
+            return value1 + value2;
+        }
+
+        const specSet = [
+            {
+                name: 'additive identity',
+                setupValues: { value1: 1, value2: 0 }
+            },
+            {
+                name: 'add two numbers',
+                setupValues: { value1: 1, value2: 2 }
+            },
+            {
+                name: 'incorrect value causes test failure',
+                setupValues: { value1: 1, value2: 'foo' }
+            }
+        ];
+
+        var message = 'Test \'incorrect value causes test failure\' failure in spec: \n' +
+            JSON.stringify(specSet[2].setupValues, null, 4) + '\n\n' +
+            'Expected result to be 1foo, but got 1';
+
+        function runSpec() {
+            quickspec
+                .verify(executeAdd)
+                .withTheorem(addTheorem)
+                .over(specSet);
+        }
+        assert.throws(runSpec, message);
+
+    });
+
+    it('should compare object values instead of pointers', function () {
+
+        const specSet = [
+            {
+                name: 'Example object',
+                setupValues: {
+                    value: {
+                        foo: 'bar',
+                        baz: 'quux',
+                        'test': {
+                            'something': 'else'
+                        }
+                    }
+                }
+            },
+            {
+                name: 'Empty object',
+                setupValues: {
+                    value: {}
+                },
+            }
+        ]
+
+        var message = 'Test \'Empty object\' failure in spec: \n';
+
+        function runSpec() {
+            quickspec
+                .verify(executeIdentity)
+                .withTheorem((setupValues) => copyValue(setupValues.value))
+                .over(specSet);
+        }
+        assert.throws(runSpec, message);
+
+    });
+
+});
+
